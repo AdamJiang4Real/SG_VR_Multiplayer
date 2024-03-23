66d314f992508cda666c0dc4471ca257c76bd967
Adam <597468319@qq.com>
2024-03-23 23:52:46 +0800

    add plugin tagpawn

---
+++ Plugins/Wutopia/Source/Wutopia/Tag/VRTagCharacter.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRTagCharacter.h"
+
+
+// Sets default values
+AVRTagCharacter::AVRTagCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	OffsetComponentToWorld = FTransform(FQuat(0.0f, 0.0f, 0.0f, 1.0f), FVector::ZeroVector, FVector(1.0f));
+}
+
+// Called when the game starts or when spawned
+void AVRTagCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVRTagCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AVRTagCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+FVector AVRTagCharacter::SetActorLocationVR(FVector NewLoc, bool bTeleport, bool bSetCapsuleLocation)
+{
+	FVector NewLocation;
+	FVector PivotOffsetVal = (bSetCapsuleLocation ? GetVRLocation_Inline() : GetProjectedVRLocation()) - GetActorLocation();
+	PivotOffsetVal.Z = 0.0f;
+
+
+	NewLocation = NewLoc - PivotOffsetVal;// +PivotPoint;// NewRotation.RotateVector(PivotPoint);
+	//NewRotation = NewRot;
+
+
+	// Also setting actor rot because the control rot transfers to it anyway eventually
+	SetActorLocation(NewLocation, false, nullptr, bTeleport ? ETeleportType::TeleportPhysics : ETeleportType::None);
+	return NewLocation - NewLoc;
+}
+
+FVector AVRTagCharacter::GetProjectedVRLocation() const
+{
+	return GetVRLocation_Inline();
+}
+
